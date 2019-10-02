e92a2ed0db01c9444c430269a80e73f93aeddeae
Bartosz Burgiel <bartek.burgiel@hotmail.com>
2019-10-02 09:44:14 +0200

    Add NewGroup constructor

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"internetCafe/tourist"
+)
 
 func main() {
 	fmt.Println("Excersise 3 - Internet cafe")
+
+	// Define structs
+	group := tourist.NewGroup()
+
+	fmt.Println(group)
+
+	// // "Main" goroutine
+	// go func() {
+	// 	for
+	// }
 }
--- tourist/group.go
+++ tourist/group.go
@@ -9,3 +9,14 @@ type Group struct {
 
 	TouristsToGo, TouristsDone int
 }
+
+// NewGroup constructor
+func NewGroup() *Group {
+	tourists := [25]*Tourist{}
+
+	for i := 0; i < 25; i++ {
+		tourists[i] = &Tourist{i + 1, 0, false, false, true}
+	}
+
+	return &Group{tourists, false, 25, 0}
+}
