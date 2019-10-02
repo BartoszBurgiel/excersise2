58e320c4c6c6d06875721a13e83088f513de7515
Bartosz Burgiel <bartek.burgiel@hotmail.com>
2019-10-02 13:49:34 +0200

    Refractor cafe package

--- cafe/computer.go
+++ cafe/computer.go
@@ -4,6 +4,7 @@ import "internetCafe/tourist"
 
 // Computer struct defines a computer from the internet cafe
 type Computer struct {
+
+	// User occupying the computer
 	User *tourist.Tourist
-	Free bool
 }
